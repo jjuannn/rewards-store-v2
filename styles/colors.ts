aa5a09dfc0b8a5076a1be7a81759bc5e94168279
juan <juancraft12yt@gmail.com>
2022-06-27 11:57:16 -0300

    Modify colors

--- styles/colors.ts
+++ styles/colors.ts
@@ -3,7 +3,7 @@ const colors = {
     900: "#252F3D",
     600: "#7C899C",
     500: "#8FA3BF",
-    300: "#252F3D",
+    300: "#DAE4F2",
     200: "#E6EDF7",
     100: "#F5F9FF",
     0: "#FFFFF",
@@ -26,7 +26,7 @@ const colors = {
     illustrationBg:
       "linear-gradient(102.47deg, #7296EB -5.34%, #EAC0E9 106.58%, #EAC0E9 106.58%)",
     sectionBg:
-      "linear-gradient(102.47deg, rgba(23, 111, 235, 0.5) -5.34%, rgba(255, 128, 255, 0.5) 106.58%)",
+      "linear-gradient(90deg, rgba(23, 111, 235, 0.5) -5.34%, rgba(255, 128, 255, 0.5) 106.58%)",
     aerolab: "linear-gradient(180deg, #FF8800 0%, #FF6600 100%);",
   },
 };
