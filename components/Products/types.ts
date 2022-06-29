11b733cbf7f4f56fb08df017736135a1aa180f3d
juan <juancraft12yt@gmail.com>
2022-06-29 15:09:58 -0300

    Create enums

---
+++ components/Products/types.ts
@@ -0,0 +1,20 @@
+enum ProductsCategories {
+  LAPTOPS = "Laptops",
+  CAMERAS = "Cameras",
+  SMART_HOME = "Smart Home",
+  AUDIO = "Audio",
+  MONITORS = "Monitors",
+  PC_ACCESSORIES = "PC Accessories",
+  GAMING = "Gaming",
+  TABLETS_AND_E_READERS = "Tablets & E-readers",
+  PHONES = "Phones",
+  DRONES = "Drones",
+}
+
+enum SortOrders {
+  MOST_RECENT = "Most Recent",
+  HIGHEST_PRICE = "Highest Price",
+  LOWEST_PRICE = "Lowest Price",
+}
+
+export { ProductsCategories, SortOrders };
