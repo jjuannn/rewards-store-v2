29c5839823acca4cc96784f6c12ada5b03c4d8cb
juan <juancraft12yt@gmail.com>
2022-07-01 11:37:48 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Tech Zone</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
