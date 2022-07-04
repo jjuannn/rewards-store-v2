3d82d99cf326772842a72e2878388ee8382f6ac0
juan <juancraft12yt@gmail.com>
2022-07-04 16:01:31 -0300

    creates API services

---
+++ service/_index.ts
@@ -0,0 +1,14 @@
+import { Axios } from "axios";
+
+const headers = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+  Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+};
+
+const axios = new Axios({
+  baseURL: "https://coding-challenge-api.aerolab.co",
+  headers,
+});
+
+export { axios };
---
+++ service/api.ts
@@ -0,0 +1,24 @@
+import { User } from "entity/user";
+import { userMapper } from "mapper/userMapper";
+import { axios } from "./_index";
+
+async function fetchUserData(): Promise<User> {
+  try {
+    const res = await axios.get("/user/me");
+    // no se xq devuelve texto plano ¿?
+    const data = JSON.parse(res.data);
+    return userMapper(data);
+  } catch (err: any) {
+    throw new Error(err.message);
+  }
+}
+
+async function addUserPoints(amount: number): Promise<any> {
+  try {
+    const res = await axios.post("/user/poins", { amount });
+    const data = JSON.parse(res.data);
+    return data;
+  } catch (err) {}
+}
+
+export { fetchUserData, addUserPoints };
