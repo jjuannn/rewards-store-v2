1b8d466c97d84b0f444d04ab012c880bbd81a755
juan <juancraft12yt@gmail.com>
2022-07-05 17:17:26 -0300

    Implement static site generation with getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,36 @@ import { MainImage } from "components/MainImage";
 import { PresentationCards } from "components/PresentationCards";
 import { PresentationTitle } from "components/PresentationTitle";
 import { ProductsSection } from "components/Products";
+import { Product } from "entity/product";
+import { productMapper } from "mapper/productMapper";
 import type { NextPage } from "next";
+import { getProducts } from "service/products";
+import { axios } from "service/_index";
 
-const Home: NextPage = () => {
+interface IHomeProps {
+  products: Product[];
+}
+
+const Home: NextPage<IHomeProps> = ({ products }) => {
   return (
     <>
       <Flex direction={{ base: "column", xl: "row" }}>
         <PresentationTitle /> <MainImage />
-      </Flex>{" "}
+      </Flex>
       <PresentationCards />
-      <ProductsSection />
+      <ProductsSection products={products} />
     </>
   );
 };
+
+export async function getStaticProps() {
+  const products = await getProducts();
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
 
 export default Home;
