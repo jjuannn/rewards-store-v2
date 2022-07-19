b6af4ba2a922ab9cb989c2c0e99cb0b35652936b
juan <juancraft12yt@gmail.com>
2022-07-19 11:44:22 -0300

    Update cypress config to avoid cypress creating a video

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     baseUrl: "http://localhost:3000",
     viewportWidth: 1920,
     viewportHeight: 1080,
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
