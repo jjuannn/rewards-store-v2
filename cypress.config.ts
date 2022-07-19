7143feb4007d203018699d584b6d963966232cff
juan <juancraft12yt@gmail.com>
2022-07-19 12:08:57 -0300

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
     video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
